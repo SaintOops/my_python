f7908f9163d76894cf42e146e7aa3eb70915b520
SaintOops <dyx.orla@gmail.com>
2020-04-25 18:18:42 +0300

    init commit

--- Project/general/general/settings.py
+++ Project/general/general/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dynamicdata',
     'contacts',
     'species',
     'django.contrib.admin',
--- Project/general/general/urls.py
+++ Project/general/general/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('species.urls')),
-    path('contacts/', include('contacts.urls'))
+    path('contacts/', include('contacts.urls')),
+    path('dynamicdata/', include('dynamicdata.urls')),
 ]

4a68854f06fa7a67a651016049a63de12d250c6c
SaintOops <dyx.orla@gmail.com>
2020-05-03 11:01:09 +0300

    init commit

--- Project/Final/general/general/urls.py
+++ Project/Final/general/general/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('species.urls')),
     path('contacts/', include('contacts.urls')),
     path('dynamicdata/', include('dynamicdata.urls')),
-    path('donate/', include('donate.urls')),
-]
+    path('donate/', include('donate.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Project/Final/general/species/models.py
+++ Project/Final/general/species/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Animal (models.Model):
+    title= models.CharField(max_length=100)
+    image= models.ImageField(upload_to='')
+    describ= models.CharField(max_length=1000)
+    date=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
